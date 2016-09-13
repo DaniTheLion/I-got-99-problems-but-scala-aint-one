96df266fc03166774d802465fcfb719b330132bb
Dani Acherkan <dani.acherkan@naturalint.com>
2016-09-13 10:53:05 +0200

    P2

---
+++ src/main/scala/p2.scala
@@ -0,0 +1,12 @@
+object P2 extends App {
+  def penultimate[T](l: List[T]): T = l match {
+    case Nil             => throw new NoSuchElementException("No penultimate element for an empty list.")
+    case x :: Nil        => throw new NoSuchElementException("No penultimate element for an empty list.")
+    case x :: y :: Nil   => x
+    case x :: xs         => penultimate(xs)
+  }
+  assert(penultimate(List(1, 1, 2, 3, 5, 8)) == 5, "Nope..")
+}
+
+
+
